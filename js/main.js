2e9ff2f6b8e02dc570a8f80021982b009d420d33
Fernando <ferelbue@gmail.com>
2024-02-04 20:57:49 +0100

    Se termina diseño mando. Se empieza con la implementacion de la barra de volumen

--- js/main.js
+++ js/main.js
@@ -17,10 +17,11 @@ let nuevoCanalReal;
 const source = document.getElementsByClassName("buttonSrc")
 let arraySource = Array.from(source)
 let sourceSelec = 0;
-
+const volumen = document.getElementsByClassName("botonVol")
+let arrayVolumen = Array.from(volumen)
 
 let video = document.getElementById("myVideo")
-
+let val = 0;
 
 
 
@@ -68,7 +69,7 @@ arrayButtons.map(
           canalActual = "canalP";
           sourceSelec = 0;
           PlayVideo()
-          function PlayVideo(){
+          function PlayVideo() {
             video.pause();
             video.src = "";
             video.load();
@@ -98,7 +99,7 @@ arrayButtons.map(
         if (("canal" + evento.target.id.slice(-1)) === "canal1") {
           canalPantalla.innerHTML = "TVE"
           PlayVideo()
-          function PlayVideo(){
+          function PlayVideo() {
             video.pause();
             video.src = "http://127.0.0.1:5500/img/video1.mp4";
             video.load();
@@ -108,7 +109,7 @@ arrayButtons.map(
         if (("canal" + evento.target.id.slice(-1)) === "canal2") {
           canalPantalla.innerHTML = "LA 2"
           PlayVideo();
-          function PlayVideo(){
+          function PlayVideo() {
             video.pause();
             video.src = "http://127.0.0.1:5500/img/video2.mp4";
             video.load();
@@ -118,7 +119,7 @@ arrayButtons.map(
         if (("canal" + evento.target.id.slice(-1)) === "canal3") {
           canalPantalla.innerHTML = "ANTENA 3"
           PlayVideo();
-          function PlayVideo(){
+          function PlayVideo() {
             video.pause();
             video.src = "http://127.0.0.1:5500/img/video3.mp4";
             video.load();
@@ -128,7 +129,7 @@ arrayButtons.map(
         if (("canal" + evento.target.id.slice(-1)) === "canal4") {
           canalPantalla.innerHTML = "QUATRO"
           PlayVideo();
-          function PlayVideo(){
+          function PlayVideo() {
             video.pause();
             video.src = "http://127.0.0.1:5500/img/video4.mp4";
             video.load();
@@ -138,7 +139,7 @@ arrayButtons.map(
         if (("canal" + evento.target.id.slice(-1)) === "canal5") {
           canalPantalla.innerHTML = "TELECINCO"
           PlayVideo();
-          function PlayVideo(){
+          function PlayVideo() {
             video.pause();
             video.src = "http://127.0.0.1:5500/img/video5.mp4";
             video.load();
@@ -148,7 +149,7 @@ arrayButtons.map(
         if (("canal" + evento.target.id.slice(-1)) === "canal6") {
           canalPantalla.innerHTML = "LA SEXTA"
           PlayVideo();
-          function PlayVideo(){
+          function PlayVideo() {
             video.pause();
             video.src = "http://127.0.0.1:5500/img/video6.mp4";
             video.load();
@@ -158,7 +159,7 @@ arrayButtons.map(
         if (("canal" + evento.target.id.slice(-1)) === "canal7") {
           canalPantalla.innerHTML = "TDP"
           PlayVideo();
-          function PlayVideo(){
+          function PlayVideo() {
             video.pause();
             video.src = "http://127.0.0.1:5500/img/video7.mp4";
             video.load();
@@ -168,7 +169,7 @@ arrayButtons.map(
         if (("canal" + evento.target.id.slice(-1)) === "canal8") {
           canalPantalla.innerHTML = "DISNEY"
           PlayVideo();
-          function PlayVideo(){
+          function PlayVideo() {
             video.pause();
             video.src = "http://127.0.0.1:5500/img/video8.mp4";
             video.load();
@@ -178,7 +179,7 @@ arrayButtons.map(
         if (("canal" + evento.target.id.slice(-1)) === "canal9") {
           canalPantalla.innerHTML = "CANAL NOU"
           PlayVideo();
-          function PlayVideo(){
+          function PlayVideo() {
             video.pause();
             video.src = "http://127.0.0.1:5500/img/video9.mp4";
             video.load();
@@ -198,10 +199,15 @@ arrayChanel.map(
     item.addEventListener("click", (evento) => {
       //Si se acaba de encender la TV la tv espera hasta que pulses un numero o un de los botones de chanel up/down
       console.log(evento.target.id.slice(-1))
-      if ((canalActual === "canalP") && (flagBoton == 1)) {
+      if ((canalActual === "canalP") && (flagBoton == 1) && ((evento.target.id) === "btnU")) {
         screen.classList.remove(screen.classList[screen.classList.length - 1])
         screen.classList.add("canal9")
         canalActual = "canal9";
+      }
+      if ((canalActual === "canalP") && (flagBoton == 1) && ((evento.target.id) === "btnD")) {
+        screen.classList.remove(screen.classList[screen.classList.length - 1])
+        screen.classList.add("canal1")
+        canalActual = "canal1";
       }
       //Boton chanel UP
       if (((evento.target.id) === "btnU") && (flagBoton == 1) && (sourceSelec == 0)) {
@@ -242,95 +248,95 @@ arrayChanel.map(
           horaPantalla.style.visibility = "hidden";
           datePantalla.style.visibility = "hidden";
         }, 3000);
-        
+
       }
 
       //Nombre de canal en pantalla
       if ((flagBoton == 1) && (sourceSelec == 0)) {
-        if (canalActual=="canal1") {
+        if (canalActual == "canal1") {
           canalPantalla.innerHTML = "TVE"
           PlayVideo()
-          function PlayVideo(){
+          function PlayVideo() {
             video.pause();
             video.src = "http://127.0.0.1:5500/img/video1.mp4";
             video.load();
             video.play();
           }
         }
-        if (canalActual=="canal2") {
+        if (canalActual == "canal2") {
           canalPantalla.innerHTML = "LA 2"
           PlayVideo();
-          function PlayVideo(){
+          function PlayVideo() {
             video.pause();
             video.src = "http://127.0.0.1:5500/img/video2.mp4";
             video.load();
             video.play();
           }
         }
-        if (canalActual=="canal3") {
+        if (canalActual == "canal3") {
           canalPantalla.innerHTML = "ANTENA 3"
           PlayVideo();
-          function PlayVideo(){
+          function PlayVideo() {
             video.pause();
             video.src = "http://127.0.0.1:5500/img/video3.mp4";
             video.load();
             video.play();
           }
         }
-        if (canalActual=="canal4") {
+        if (canalActual == "canal4") {
           canalPantalla.innerHTML = "QUATRO"
           PlayVideo();
-          function PlayVideo(){
+          function PlayVideo() {
             video.pause();
             video.src = "http://127.0.0.1:5500/img/video4.mp4";
             video.load();
             video.play();
           }
         }
-        if (canalActual=="canal5") {
+        if (canalActual == "canal5") {
           canalPantalla.innerHTML = "TELECINCO"
           PlayVideo();
-          function PlayVideo(){
+          function PlayVideo() {
             video.pause();
             video.src = "http://127.0.0.1:5500/img/video5.mp4";
             video.load();
             video.play();
           }
         }
-        if (canalActual=="canal6") {
+        if (canalActual == "canal6") {
           canalPantalla.innerHTML = "LA SEXTA"
           PlayVideo();
-          function PlayVideo(){
+          function PlayVideo() {
             video.pause();
             video.src = "http://127.0.0.1:5500/img/video6.mp4";
             video.load();
             video.play();
           }
         }
-        if (canalActual=="canal7") {
+        if (canalActual == "canal7") {
           canalPantalla.innerHTML = "TDP"
           PlayVideo();
-          function PlayVideo(){
+          function PlayVideo() {
             video.pause();
             video.src = "http://127.0.0.1:5500/img/video7.mp4";
             video.load();
             video.play();
           }
         }
-        if (canalActual=="canal8") {
+        if (canalActual == "canal8") {
           canalPantalla.innerHTML = "DISNEY"
           PlayVideo();
-          function PlayVideo(){
+          function PlayVideo() {
             video.pause();
             video.src = "http://127.0.0.1:5500/img/video8.mp4";
             video.load();
             video.play();
           }
         }
-        if (canalActual=="canal9") {
+        if (canalActual == "canal9") {
           canalPantalla.innerHTML = "CANAL NOU"
           PlayVideo();
-          function PlayVideo(){
+          function PlayVideo() {
             video.pause();
             video.src = "http://127.0.0.1:5500/img/video9.mp4";
             video.load();
@@ -346,11 +352,15 @@ arrayChanel.map(
 arraySource.map(
   item => {
     item.addEventListener("click", (evento) => {
+
       sourceSelec++;
 
       if ((sourceSelec == 1) && (flagBoton == 1)) {
+        screen.classList.remove(screen.classList[screen.classList.length - 1])
+        screen.classList.add("canalS1")
+        canalActual = "canalS1"
         PlayVideo();
-        function PlayVideo(){
+        function PlayVideo() {
           video.pause();
           video.src = "http://127.0.0.1:5500/img/videoH1.mp4";
           video.load();
@@ -361,37 +371,43 @@ arraySource.map(
         sourcePantalla.style.visibility = "visible";
         datePantalla.style.visibility = "visible";
         horaPantalla.style.visibility = "visible";
-          //Fecha y hora permanecen durante 3 segundos
-          setTimeout(function () {
-            sourcePantalla.style.visibility = "hidden";
-            datePantalla.style.visibility = "hidden";
-            horaPantalla.style.visibility = "hidden";
-          }, 3000);
+        //Fecha y hora permanecen durante 3 segundos
+        setTimeout(function () {
+          sourcePantalla.style.visibility = "hidden";
+          datePantalla.style.visibility = "hidden";
+          horaPantalla.style.visibility = "hidden";
+        }, 3000);
       }
-      if ((sourceSelec == 2) && (flagBoton == 1))  {
+      if ((sourceSelec == 2) && (flagBoton == 1)) {
+        screen.classList.remove(screen.classList[screen.classList.length - 1])
+        screen.classList.add("canalS2")
+        canalActual = "canalS2"
 
         sourcePantalla.innerHTML = "HDMI-2";
         canalPantalla.innerHTML = ""
         sourcePantalla.style.visibility = "visible";
         datePantalla.style.visibility = "visible";
         horaPantalla.style.visibility = "visible";
-          //Fecha y hora permanecen durante 3 segundos
-          setTimeout(function () {
-            sourcePantalla.style.visibility = "hidden";
-            datePantalla.style.visibility = "hidden";
-            horaPantalla.style.visibility = "hidden";
-          }, 3000);
-        }
+        //Fecha y hora permanecen durante 3 segundos
+        setTimeout(function () {
+          sourcePantalla.style.visibility = "hidden";
+          datePantalla.style.visibility = "hidden";
+          horaPantalla.style.visibility = "hidden";
+        }, 3000);
         PlayVideo();
-        function PlayVideo(){
+        function PlayVideo() {
           video.pause();
           video.src = "http://127.0.0.1:5500/img/videoH2.mp4";
           video.load();
           video.play();
         }
-      if ((sourceSelec == 3) && (flagBoton == 1))  {
+      }
+
+      if ((sourceSelec == 3) && (flagBoton == 1)) {
+        screen.classList.remove(screen.classList[screen.classList.length - 1])
+        screen.classList.add("canal1");
         PlayVideo();
-        function PlayVideo(){
+        function PlayVideo() {
           video.pause();
           video.src = "http://127.0.0.1:5500/img/video1.mp4";
           video.load();
@@ -404,13 +420,44 @@ arraySource.map(
         sourcePantalla.style.visibility = "visible";
         datePantalla.style.visibility = "visible";
         horaPantalla.style.visibility = "visible";
-          //Fecha y hora permanecen durante 3 segundos
-          setTimeout(function () {
-            sourcePantalla.style.visibility = "hidden";
-            datePantalla.style.visibility = "hidden";
-            horaPantalla.style.visibility = "hidden";
-          }, 3000);
+        //Fecha y hora permanecen durante 3 segundos
+        setTimeout(function () {
+          sourcePantalla.style.visibility = "hidden";
+          datePantalla.style.visibility = "hidden";
+          horaPantalla.style.visibility = "hidden";
+        }, 3000);
       }
+    })
+  }
+)
+
+
+
+
+
+arrayVolumen.map(
+  item => {
+    item.addEventListener("click", (evento) => {
+
+      console.log(evento.target.id.slice(-1))
+      if(evento.target.id.slice(-1)=="+"){
+
+        val++;
+      }
+      if(evento.target.id.slice(-1)=="-"){
+
+        val--;
+      }
+
+      window.SetVolume = function()
+      {
+          var player = document.getElementById('video');
+          console.log('Before: ' + player.volume);
+          player.volume = val / 100;
+          console.log('After: ' + player.volume);
+      }
+
+
     })
   }
 )
